efb65ece7e280c75c0e609c863e6f51d9e309a22
NodeReplacer <francismlee@hotmail.com>
2023-02-12 02:04:56 -0500

    Adding previous/next buttons
    Maybe I'll add a number for the user to know what page they're on.

--- js/GameAPIFetcher.js
+++ js/GameAPIFetcher.js
@@ -19,9 +19,80 @@ previous = the https location for the previous batch that fit the request
 results = An array of 20 objects that are the list of games. Each game object also has their own values.
 */
 
+let API_URL_RAWG;
+let API_KEY_RAWG;
 
-let API_URL_RAWG = '';
-let API_KEY_RAWG = '';
+let nextURL;
+let prevURL;
+
+let prevButtons = document.getElementsByClassName("prev-button");
+let nextButtons = document.getElementsByClassName("next-button");
+
+//Get the bottom button container and remove its border.
+let bottomButtonContainer = document.getElementById("bottomButtonContainer");
+bottomButtonContainer.style.borderBottom = "none";
+
+//Interestingly. Putting these steps into a function causes a noticeable slowdown.
+
+function updateGameList(gameList, data) {
+    for (let APIresult of data.results) {
+        let gameItem = document.createElement('div');
+        gameItem.className = "game";
+        gameList.appendChild(gameItem);
+        let gameName = document.createElement('div');
+        gameName.className = 'game-name';
+        gameName.innerHTML = APIresult.name;
+        gameItem.appendChild(gameName);
+        let gameRelease = document.createElement('div');
+        gameRelease.className = "release-date";
+        gameRelease.innerHTML = `Release Date: ${APIresult.released}`;
+        gameItem.appendChild(gameRelease);
+    }
+
+    prevURL = data.previous;
+    nextURL = data.next;
+}
+
+function changePage(targetURL) {
+    //To update the list we fetch the new page at the url pointed to by either next or previous. (builtURL)
+    //We can pass this data through arguments instead of finding it here.
+
+    //But what do I want to do? I want to update the page with the new info. Maybe I can also take out work
+    //sitting at .then(data => {etc})?
+
+    //If I did that then I need to pass the dataResults in. Then I change the variables storing the next and prev pages
+    //to the new position we are at.
+    //To get that next and prev values I need to have fetched the next/prev from the destination page.
+    //I will also need to fetch the results and update the data I have in front of me.
+    //Should I attach the fetch to the event listeners or do I just fetch in this function. Both buttons
+    //will be fetching after all.
+    console.log(`Clicked ${targetURL}`)
+
+    //The once we hit the end targetURL is null. So we return null.
+    if (targetURL === null) {
+        return null;
+    }
+
+    //If we have not hit the end then fetch the new data and replace the old data.
+    fetch(targetURL)
+        .then(response => response.json())
+        .then (data => {
+            //Get game list container.
+            const gameList = document.getElementById(`game-list`);
+            //Empty out the game list container. By checking if the first child exists.
+            //while looping as long as that is true.
+            while (gameList.firstChild) {
+                //And removing whatever is currently occupying the spot of firstChild.
+                //This will require the list to relink itself every time. It may be better to use a for loop
+                gameList.removeChild(gameList.firstChild);
+            }
+
+            updateGameList(gameList, data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
 
 //Overview of this promise statement. This code waits for API_URL_RAWG to be returned before resolving so that
 //the code can rely on API_URL_RAWG being built and pointing to an actual location.
@@ -62,6 +133,7 @@ apiUrlBuilder
         console.log(`builtURL: ${builtURL}`);
         fetch(builtURL)
             .then(response => {
+                //The URL we were given was not found.
                 if (response.status === 404) {
                     throw new Error(`URL at ${builtURL} not found.`);
                 }
@@ -70,13 +142,18 @@ apiUrlBuilder
                 }
             })
             .then(data => {
-                //Otherwise we need to organize this data now. Just use the results array.
-                console.log(data.results);
+                //Organize the data
+                console.log(data);
                 const gameList = document.querySelector(`.game-list`);
+
+                updateGameList(gameList, data);
+
+                /*
                 for (let APIresult of data.results) {
                     let gameItem = document.createElement('div');
                     gameItem.className = "game";
                     gameList.appendChild(gameItem);
+                    //gameList.insertBefore(gameItem,newButtonContainer);
                     let gameName = document.createElement('div');
                     gameName.className = 'game-name';
                     gameName.innerHTML = APIresult.name;
@@ -85,6 +162,24 @@ apiUrlBuilder
                     gameRelease.className = "release-date";
                     gameRelease.innerHTML = `Release Date: ${APIresult.released}`;
                     gameItem.appendChild(gameRelease);
+                }
+
+                prevURL = data.previous;
+                nextURL = data.next;
+                */
+            })
+            .then(function() {
+                //Wait for the data to be taken and displayed. Then update the functionality
+                //of the buttons.
+                for (let i = 0; i < prevButtons.length; ++i) {
+                    prevButtons[i].addEventListener("click",function() {
+                        changePage(prevURL);
+                    })
+                }
+                for (let i = 0; i < nextButtons.length; ++i) {
+                    nextButtons[i].addEventListener("click",function() {
+                        changePage(nextURL);
+                    })
                 }
             })
     })
